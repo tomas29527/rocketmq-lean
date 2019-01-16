879093fd1a0b90876fac57d8780ce620324d306f
e <aaabbb@163.com>
2019-01-16 11:40:00 +0800

    rocketmq 学习

---
+++ src/main/java/com/you/rocketmq/RocketmqLeanApplication.java
@@ -0,0 +1,14 @@
+package com.you.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqLeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqLeanApplication.class, args);
+    }
+
+}
+
